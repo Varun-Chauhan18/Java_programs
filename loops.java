7afa7332ff79626eaf9d89016ac38cdcf9c4ee7b
Varun Chauhan <chauhanvarun093@gmail.com>
2025-06-09 22:57:34 +0530

    files added

---
+++ loops.java
@@ -0,0 +1,18 @@
+public class loops {
+    public static void main(String[] args) {
+        // for loops
+        // iteration are specified and runs the code for a particular number of times
+        // for (int i = 1; i <= 10; i++) {
+        // System.out.println("hi " + i);
+        // }
+
+        // while loop
+        // iterate through the loops until the given condition is true
+        int num = 0;
+        while (num < 10) {
+            System.out.println("hello");
+            num++;
+        }
+
+    }
+}
