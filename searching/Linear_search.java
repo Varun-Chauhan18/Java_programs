ad7f2a054322bc86b91a70ba80832f8dabefef7b
Varun Chauhan <chauhanvarun093@gmail.com>
2025-06-12 04:09:46 +0530

    folder added with files

---
+++ searching/Linear_search.java
@@ -0,0 +1,23 @@
+// checks every element one by one until the target element is found 
+public class Linear_search {
+    public static int linearSearch(int[] arr, int target) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == target) {
+                return i; // Target found at index i
+            }
+        }
+        return -1; // Target not found
+    }
+
+    public static void main(String[] args) {
+        int[] data = { 10, 25, 30, 45, 50 };
+        int target = 30;
+
+        int result = linearSearch(data, target);
+        if (result == -1) {
+            System.out.println("Element not found.");
+        } else {
+            System.out.println("Element found at index: " + result);
+        }
+    }
+}
